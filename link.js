9f755361afba1bf81f9e01e6c5966bbb1a32a540
Espen Schulstad <espen@Espens-iMac.local>
2014-07-16 22:30:00 +0200

    dd

--- link.js
+++ link.js
@@ -24,13 +24,25 @@ if (Meteor.isClient) {
       if(addressLineContains("id=")){
         console.log("Id found. Fetching route.")
         routeId = addressLineIdGrabber();
-        route = Route.find({_id:routeId})
+        route = Route.findOne({_id:routeId});
+        console.log("Points registered:");
+        console.log(route);
       } else {
-        routeId = Route.insert({points:[]})
+        routeId = Route.insert({'points':[]})
         window.location.href="id="+routeId;
         console.log("No id found. Creating route.")
       }
   }
+
+  var containsPoint = function(a, p) {
+    for (var i = 0; i < a.length; i++) {
+        if (a[i].B === p.B && a[i].k === p.k) {
+            return true;
+        }
+    }
+    return false;
+  }
+
 
 
   var showPositionOnMap = function(){
@@ -38,15 +50,17 @@ if (Meteor.isClient) {
       navigator.geolocation.getCurrentPosition(function(position) {
         pos = new google.maps.LatLng(position.coords.latitude,
                                          position.coords.longitude);
-        console.log("Current position is latitude:" +position.coords.latitude + " and longitude" + position.coords.longitude);
+        console.log("Current position is latitude: " +position.coords.latitude + " and longitude" + position.coords.longitude);
 
         var infowindow = new google.maps.InfoWindow({
           map: map,
           position: pos,
           content: 'Du er her.'
         });
-
-        route.points.push(pos); 
+        route = Route.findOne({_id:routeId});
+        if(!containsPoint(route.points,pos)){
+          route.points.push(pos); 
+        }
         Route.update({_id:route._id},route);
 
         map.setCenter(pos);
@@ -60,42 +74,10 @@ if (Meteor.isClient) {
 
 
 
-  Template.menu.events({
-    'click #show-me-on-the-map' : function (event) {
-        console.log("Show me on the map.");
-        document.getElementById("menu").className="hidden";
-        showPositionOnMap();
-    }
-  } );
-
-
-  Meteor.startup(function () {
-
-    drawMap();
-    // code to run on client at startup
-    // if id is present in window.location.href
-    // find points with id
-    // render splash screen with participant count + show your own location.
-    resolveAppState();
-
-
-    // ask to share location
-    // if no id is present in window.location.href
-    // render splashscreen with "Start link"
-    // Steps. 1. Register on map 2. Share link to friends.
-
-
-  });
-
-
-
-  var drawDirectionsOnMap = function(){
+  var drawDirectionsOnMap = function(p1,p2){
       if(directionsDisplay.getMap() === null){
         directionsDisplay.setMap(map);      
       }
-
-        p1 = origin;
-        p2 = destination;
 
       var request = {
         origin: p1,
@@ -107,8 +89,6 @@ if (Meteor.isClient) {
         if (status == google.maps.DirectionsStatus.OK) {
           lastRoute = response.routes[0].overview_path  
           directionsDisplay.setDirections(response);
-        } else {
-          console.log(response);
         }
       });
   }
@@ -143,6 +123,44 @@ if (Meteor.isClient) {
   }
 
 
+
+  Template.menu.events({
+    'click #show-me-on-the-map' : function (event) {
+        console.log("Show me on the map.");
+        document.getElementById("menu").className="hidden";
+        showPositionOnMap();
+    }
+  } );
+
+
+  Meteor.startup(function () {
+    drawMap();
+    // code to run on client at startup
+    // if id is present in window.location.href
+    // find points with id
+    // render splash screen with participant count + show your own location.
+    resolveAppState();
+    // ask to share location
+    // if no id is present in window.location.href
+    // render splashscreen with "Start link"
+    // Steps. 1. Register on map 2. Share link to friends.
+  });
+
+
+
+  var directionsUpdate = function(){
+    route = Route.findOne({_id:routeId});
+    console.log(route)
+    if(route.points.length > 1 && route.points.length % 2 === 0)
+    for (var i = 0; i < route.points.length; i++) {
+      var p1 = new google.maps.LatLng(route.points[i].k,   route.points[i].B);
+      var p2 = new google.maps.LatLng(route.points[i+1].k, route.points[i+1].B);
+      drawDirectionsOnMap(p1,p2);
+    };
+  }
+
+
+  Meteor.setInterval(directionsUpdate, 10000); // Add new trains to track
 
 
 
